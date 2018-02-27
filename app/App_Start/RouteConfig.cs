fa309d834455958f04b711710809f044acc327b7
Lucas juliano <lucas.juliano@live.com>
2018-02-27 19:17:25 -0300

    angular with mvc

--- app/App_Start/RouteConfig.cs
+++ app/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace app
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+              name: "Angular Routing",
+              url: "{*url}",
+              defaults: new { controller = "Home", action = "Index" }
+          );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
